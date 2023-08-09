9ecf896a6b5aff3cc394bec64abeb6eddbc24681
Svetlana Naumova <DrSvetaNaumova@yandex.ru>
2023-08-09 14:01:03 +0300

    sprint15-draft0.1

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -135,7 +135,8 @@ class Api {
 
 const api = new Api({
   // baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-65',
-  baseUrl: 'http://localhost:3000',
+  // baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://drsvetanaumova.nomoreparties.co',
   headers: {
     // authorization: 'e5c7629d-174e-499d-9501-41f48afec7e6',
     'Content-Type': 'application/json',
