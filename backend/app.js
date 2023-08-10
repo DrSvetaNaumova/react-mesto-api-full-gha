7420782df39f59770f06c4c6397d974a8f2637b1
Svetlana Naumova <DrSvetaNaumova@yandex.ru>
2023-08-10 14:23:51 +0300

    sprint15-draft0.14

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const router = require('./routes/index');
 
 app.use(express.json());
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'https://drsvetanaumova.nomoreparties.co', 'http://drsvetanaumova.nomoreparties.co'] }));
+app.use(cors({ origin: ['https://drsvetanaumova.nomoreparties.co', 'http://drsvetanaumova.nomoreparties.co'] }));
 
 app.use(requestLogger);
 
