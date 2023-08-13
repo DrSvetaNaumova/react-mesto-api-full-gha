f534a5df78b937384b18f21d5cbd12ede92d99d7
Svetlana Naumova <DrSvetaNaumova@yandex.ru>
2023-08-13 15:12:33 +0300

    sprint15-draft3

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,26 @@
-const express = require('express');
-
-const cors = require('cors');
-
 require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors');
+
 const { PORT = 3000 } = process.env;
-
 const app = express();
-
+const helmet = require('helmet');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { errorHandler, logErrors } = require('./middlewares/errors');
+const router = require('./routes/index');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-const { errors } = require('celebrate');
-
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-
-const { errorHandler, logErrors } = require('./middlewares/errors');
-
-const router = require('./routes/index');
-
 app.use(express.json());
-
 app.use(cors({ origin: ['https://drsvetanaumova.nomoreparties.co', 'http://drsvetanaumova.nomoreparties.co'] }));
-
 app.use(requestLogger);
-
+app.use(helmet());
 app.use(router);
-
 app.use(errorLogger);
-
 app.use(errors());
-
 app.use(logErrors, errorHandler);
 
 app.listen(PORT, () => {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,17 +4,23 @@ const router = express.Router();
 
 const auth = require('../middlewares/auth');
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// удалить краш-тест после успешного прохождения ревью
+// router.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
 router.post('/signup', require('./signup'));
 router.post('/signin', require('./login'));
 
-router.use('/users', auth, require('./users'));
-router.use('/cards', auth, require('./cards'));
+router.use(auth); // NEW
+
+// router.use('/users', auth, require('./users'));
+// router.use('/cards', auth, require('./cards'));
+
+router.use('/users', require('./users'));
+router.use('/cards', require('./cards'));
 
 router.use('*', require('./page404'));
 
